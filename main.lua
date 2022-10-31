7cb5a2c7dfa5eea178d66902f2352d5de8294ac9
Arthur-Le-M <arthurlemenn29870@gmail.com>
2022-10-31 14:41:54 +0100

    Ajout de la balle

--- main.lua
+++ main.lua
@@ -6,8 +6,19 @@ raquette.width = 20
 raquette.height = 80
 raquette.speed = 1
 
+-- Balle
+balle = {}
+balle.x = 0
+balle.y = 0
+balle.width = 10
+balle.height = 10
+balle.speedX = 2
+balle.speedY = 2
 
 function love.load()
+    -- Centrage de la balle au début de la partie
+    balle.x = love.graphics.getWidth()/2 + balle.width/2
+    balle.y = love.graphics.getHeight()/2 + balle.height/2
 end
 
 function love.update()
@@ -22,4 +33,7 @@ end
 function love.draw()
     -- Graphics de la raquette
     love.graphics.rectangle("fill", raquette.x, raquette.y, raquette.width, raquette.height)
+
+    --Graphics de la balle
+    love.graphics.rectangle("fill", balle.x, balle.y, balle.width, balle.height)
 end
