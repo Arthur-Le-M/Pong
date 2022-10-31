11b93544f48996187a34b1ae79ef4a8a7e361352
Arthur-Le-M <arthurlemenn29870@gmail.com>
2022-10-31 15:05:50 +0100

    Correction collision raquette

--- main.lua
+++ main.lua
@@ -12,13 +12,12 @@ balle.x = 0
 balle.y = 0
 balle.width = 10
 balle.height = 10
-balle.speedX = 1
-balle.speedY = 1
+balle.speedX = 3
+balle.speedY = 3
 
 function love.load()
     -- Centrage de la balle au début de la partie
-    balle.x = love.graphics.getWidth()/2 + balle.width/2
-    balle.y = love.graphics.getHeight()/2 + balle.height/2
+    centrageBalle(balle)
 end
 
 function love.update()
@@ -33,14 +32,19 @@ function love.update()
     balle.x = balle.x + balle.speedX
     balle.y = balle.y + balle.speedY
 
-    -- Effet de rebond sur les murs pour la balle
+    -- Effet de rebond sur les murs
     -- Pour la ligne X
-    if balle.x + balle.width >= love.graphics.getWidth() or balle.x <= raquette.x + raquette.width then
+    if balle.x + balle.width >= love.graphics.getWidth()then
         balle.speedX = -balle.speedX
     end
     -- Pour la ligne Y
     if balle.y + balle.height >= love.graphics.getHeight() or balle.y <= 0 then
         balle.speedY = -balle.speedY
+    end
+
+    -- Effet de rebond sur la raquette
+    if balle.x <= raquette.x + raquette.width and balle.y <= raquette.y + raquette.height and balle.y >= raquette.y then
+        balle.speedX = -balle.speedX
     end
 end
 
@@ -50,4 +54,9 @@ function love.draw()
 
     --Graphics de la balle
     love.graphics.rectangle("fill", balle.x, balle.y, balle.width, balle.height)
+end
+
+function centrageBalle(balle)
+    balle.x = love.graphics.getWidth()/2 + balle.width/2
+    balle.y = love.graphics.getHeight()/2 + balle.height/2
 end
